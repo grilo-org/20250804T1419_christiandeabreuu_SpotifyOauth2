c62a55594a1a0d681c18431fb21b8e9af565454d
christian <christiantrovao@gmail.com>
2025-05-22 06:54:48 -0300

    responsividade celular e tablet

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.google.services) apply false
+    alias(libs.plugins.crashlytics) apply false
     id("com.google.devtools.ksp") version "1.9.21-1.0.15" apply false
 
 }
