fdb1dbbb7d9666bf016649f9533d0e776e7a75ac
christian <christiantrovao@gmail.com>
2025-06-06 17:37:57 -0300

    ajustes classes de testes

--- app/src/test/java/com/example/spotifyapi/app/ui/topartists/TopArtistsViewModelTest.kt
+++ app/src/test/java/com/example/spotifyapi/app/ui/topartists/TopArtistsViewModelTest.kt
@@ -2,19 +2,29 @@ package com.example.spotifyapi.app.ui.topartists
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
-import io.mockk.*
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.*
-import org.junit.*
-import com.example.spotifyapi.app.domain.usecase.GetUserProfileUseCase
-import com.example.spotifyapi.app.domain.usecase.GetTopArtistsUseCase
-import com.example.spotifyapi.app.data.model.ArtistResponse
 import com.example.spotifyapi.app.data.model.Image
-import com.example.spotifyapi.app.data.model.TopArtistsResponse
 import com.example.spotifyapi.app.data.model.UserProfile
-import com.example.spotifyapi.app.data.paging.ArtistPagingSource
-import junit.framework.TestCase.assertNotNull
+import com.example.spotifyapi.app.domain.usecase.GetTopArtistsUseCase
+import com.example.spotifyapi.app.domain.usecase.GetUserProfileUseCase
+import com.example.spotifyapi.utils.Constants
+import io.mockk.Runs
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.verify
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.TestScope
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class TopArtistsViewModelTest {
@@ -22,20 +32,21 @@ class TopArtistsViewModelTest {
     @get:Rule
     val instantTaskExecutorRule = InstantTaskExecutorRule()
 
+    // Para rodar coroutines no thread de teste
     private val testDispatcher = StandardTestDispatcher()
-    private lateinit var viewModel: TopArtistsViewModel
-    private val getUserProfileUseCase: GetUserProfileUseCase = mockk()
-    private val artistPagingSource: ArtistPagingSource = mockk()
-    private val getTopArtistsUseCase: GetTopArtistsUseCase = mockk()
+    private val testScope = TestScope(testDispatcher)
     private val userProfileObserver: Observer<UserProfile?> = mockk(relaxed = true)
-    private val errorObserver: Observer<String> = mockk(relaxed = true)
+
+
+    private val getUserProfileUseCase: GetUserProfileUseCase = mockk(relaxed = true)
+    private val getTopArtistsUseCase: GetTopArtistsUseCase = mockk(relaxed = true)
+
+    private lateinit var viewModel: TopArtistsViewModel
 
     @Before
     fun setup() {
         Dispatchers.setMain(testDispatcher)
-        viewModel = TopArtistsViewModel(getUserProfileUseCase, artistPagingSource, getTopArtistsUseCase)
-        viewModel.userProfileLiveData.observeForever(userProfileObserver)
-        viewModel.errorLiveData.observeForever(errorObserver)
+        viewModel = TopArtistsViewModel(getUserProfileUseCase, getTopArtistsUseCase)
     }
 
     @After
@@ -43,54 +54,51 @@ class TopArtistsViewModelTest {
         Dispatchers.resetMain()
     }
 
-    // 🔹 Testando busca bem-sucedida do perfil do usuário
     @Test
-    fun `getUserProfile should update userProfileLiveData when use case returns data`() = runTest {
-        //Given
-        val mockkListImages: List<Image> =
-            listOf(mockk(relaxed = true), mockk(relaxed = true), mockk(relaxed = true))
-        val fakeUserProfile = UserProfile("1", "User Name", mockkListImages)
-        coEvery { getUserProfileUseCase.getUserProfile() } returns fakeUserProfile
+    fun `getUserProfile posts value to userProfileLiveData on success`() = runTest {
+        // Arrange
+        val userProfile = UserProfile("user_id", "User", emptyList())
+        coEvery { getUserProfileUseCase.getUserProfile() } returns userProfile
 
-        //When
+        val observer = mockk<Observer<UserProfile?>>(relaxed = true)
+        viewModel.userProfileLiveData.observeForever(observer)
+
+        // Act
         viewModel.getUserProfile()
-        advanceUntilIdle()
+        testDispatcher.scheduler.advanceUntilIdle()
 
-        //Then - Teste de busca bem-sucedida do perfil do usuário
-        verify { userProfileObserver.onChanged(fakeUserProfile) }
-        coVerify(exactly = 1) { getUserProfileUseCase.getUserProfile()}
+        // Assert
+        verify { observer.onChanged(userProfile) }
+        viewModel.userProfileLiveData.removeObserver(observer)
     }
 
     @Test
-    fun `getUserProfile should update errorLiveData when use case throws exception`() = runTest {
-        //Given
-        coEvery { getUserProfileUseCase.getUserProfile() } throws Exception("Erro ao buscar perfil do usuário")
+    fun `getUserProfile posts error to errorLiveData on failure`() = runTest {
+        // Arrange
+        coEvery { getUserProfileUseCase.getUserProfile() } throws RuntimeException("error")
 
-        //When
+        val observer = mockk<Observer<String>>(relaxed = true)
+        viewModel.errorLiveData.observeForever(observer)
+
+        // Act
         viewModel.getUserProfile()
-        advanceUntilIdle() // 🔹 Aguarda execução das corrotinas
+        testDispatcher.scheduler.advanceUntilIdle()
 
-        //Then - teste de erro na busca do perfil do usuário
-        verify { errorObserver.onChanged("Erro ao buscar perfil do usuário") }
-        coVerify(exactly = 1) { getUserProfileUseCase.getUserProfile() }
+        // Assert
+        verify { observer.onChanged(any()) }
+        viewModel.errorLiveData.removeObserver(observer)
     }
 
     @Test
-    fun `getArtistsPagingData should return valid PagingData`() = runTest {
-        //Given
-        val fakeArtists = listOf(
-            ArtistResponse("1", "Artist 1", 90, emptyList()),
-            ArtistResponse("2", "Artist 2", 80, emptyList())
-        )
+    fun `preloadOfflineArtists calls useCase method`() = runTest {
+        // Arrange
+        coEvery { getTopArtistsUseCase.preloadAllTopArtists(Constants.MEDIUM_TERM) } just Runs
 
-        coEvery { getTopArtistsUseCase.fetchAndSaveTopArtists(any(), any()) } returns TopArtistsResponse(
-            items = fakeArtists, total = 2, limit = 20, offset = 0, href = "href", next = "next", previous = "previous"
-        )
-        //When
+        // Act
+        viewModel.preloadOfflineArtists()
+        testDispatcher.scheduler.advanceUntilIdle()
 
-        val result = viewModel.getArtistsPagingData()
-
-        //Then - Verifica se o valor nao é nulo
-        assertNotNull(result)
+        // Assert
+        coVerify { getTopArtistsUseCase.preloadAllTopArtists(Constants.MEDIUM_TERM) }
     }
 }
